b9baf8b363fdc85280f41d876e8e71c63f300f8b
VLTN0 <gfree6311@gmail.com>
2023-08-24 12:27:01 +0100

    Made title work
    Adding button..

--- index.js
+++ index.js
@@ -1,9 +1,36 @@
-const left = document.getElementById("left-side");
+const leftSide = document.getElementById("left-side");
+const fancyText = document.querySelector(".fancy-text");
+const special = document.querySelector(".special");
 
-const handleMove = e => {
-  left.style.width = `${e.clientX / window.innerWidth * 100}%`;
-}
+const handleOnMove = (e) => {
+    const x = (e.clientX / window.innerWidth) * 100;
 
-document.onmousemove = e => handleMove(e);
+    const keyframes = {
+        width: `${x}%`
+    };
 
-document.ontouchmove = e => handleMove(e.touches[0]);
+    leftSide.animate(keyframes, {
+        duration: 800,
+        fill: "forwards"
+    });
+};
+
+document.onmousemove = (e) => handleOnMove(e);
+
+document.ontouchmove = (e) => handleOnMove(e.touches[0]);
+
+document.addEventListener("DOMContentLoaded", () => {  
+    document.getElementByClassName("btn-6")
+      .addEventListener('mouseenter', (e) => {
+              let parentOffset = this.offset(),
+                relX = e.pageX - parentOffset.left,
+                relY = e.pageY - parentOffset.top;
+              this.find('span').css({top:relY, left:relX});
+      })
+      .addEventListener('mouseout', (e) => {
+              let parentOffset = this.offset(),
+                relX = e.pageX - parentOffset.left,
+                relY = e.pageY - parentOffset.top;
+          this.find('span').css({top:relY, left:relX});
+      });
+  });
